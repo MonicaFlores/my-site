fa7cba6651cb55239e4fa6c763ccbb441b6018f8
mf3435 <mf3435@nyu.edu>
2018-02-01 20:02:55 -0500

    add javascript examples

--- class-2/js/scripts.js
+++ class-2/js/scripts.js
@@ -11,3 +11,37 @@ $('.link1').click(function() {
 
   location.replace(randomOption);
   })
+
+count=1;
+//Number
+console.log(typeof(count));
+
+var myName = 'Monica Flores';
+// string
+console.log(typeof(myName));
+
+//object
+var myObject = {
+  myLastName: 'Flores',
+  myFullName: myName,
+  someNumber: 8893,
+}
+
+//Loads the whole object
+console.log(myObject)
+// Loads only someNumber within my object
+console.log(myObject.someNumber)
+
+// array
+var myArrayOfNumbers = [15, 88, 01, 103];
+//1st element is listed #0
+console.log(myArrayOfNumbers[0])
+
+var thingsILike = ['Gis/Maps', 'Architecture', 'Urban Design and Planning']
+
+thingsILike.forEach(function(thing){
+  console.log(things);
+  //Make an object in htmal
+  $('.like-list').append('<li>' +thing+ '</li>')
+}
+)
