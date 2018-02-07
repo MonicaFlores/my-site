1f6d4db7b487a7a03f2c541a6e7140762c976b25
Monica Flores <33554855+MonicaFlores@users.noreply.github.com>
2018-02-07 15:22:55 -0500

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -3,3 +3,8 @@ $("button").click(function(){
         $("#div2").fadeToggle("slow");
         $("#div3").fadeToggle("slow");
     });
+
+$(".button2").click(function(){
+        $("#div4").fadeToggle();
+        $("#div5").fadeToggle("slow");
+    });
