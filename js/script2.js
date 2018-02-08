265db20ce0fde86d2da964e73f7eed7e25439adf
Flores <mf3435@ad.law.nyu.edu>
2018-02-08 11:29:50 -0500

    Add image modal

--- js/script2.js
+++ js/script2.js
@@ -8,3 +8,34 @@ $(".button2").click(function(){
         $("#div4").fadeToggle();
         $("#div5").fadeToggle("slow");
     });
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption - map 1
+var img = document.getElementById('map1');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption - map 2
+var img = document.getElementById('map2');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
